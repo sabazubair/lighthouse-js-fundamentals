af48d8caaf315ce1d26cfd6746a3694426522052
Saba Zubair <saba@lighthouselabs.com>
2018-12-29 01:50:15 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,9 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+packingList[3];
+/* This will output pans. */
